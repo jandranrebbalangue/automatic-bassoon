af7868e8de229511aea25a87e92fba718d8ffe57
Jan Dranreb Balangue <jandranrebbalangue@Jan.local>
2025-06-23 17:22:37 +0800

    Add branch name

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
+if [ -z "$1" ]; then
+  echo "⚠️   No branch argument passed. Defaulting to 'main'."
+  BRANCH="main"
+else
+  BRANCH="$1"
+fi
+
+echo "🚀 Deploying branch: $BRANCH"
+
+git fetch origin
+
+if git show-ref --verify --quiet refs/heads/$BRANCH; then
+  git checkout $BRANCH
+else
+  git checkout -b $BRANCH origin/$BRANCH
+fi
+
+git pull origin $BRANCH
+
 docker compose -f docker-compose.yml up -d --build --force-recreate node-app
 
 docker image prune -f
