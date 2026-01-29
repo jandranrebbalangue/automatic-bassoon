baff82dc750e2f51939fc8c432db734088fb7e4e
Jan Dranreb Balangue <jandranrebbalangue@Jan.local>
2026-01-29 13:55:59 +0800

    Add console log for testing

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import { createServer } from "node:http";
 
 const server = createServer((req, res) => {
 	res.writeHead(200, { "Content-Type": "text/plain" });
+	console.log(`Received request for ${req.url}`);
 	res.end("Hello World!\n");
 });
 
